7b42276b4fc6d18f771a67d6f47de5139fd12ec2
gunkev <kevinenzapdi@gmail.com>
2023-06-26 18:35:21 +0100

    created select component

---
+++ src/components/Selector.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useTolgee } from '@tolgee/vue'
+const tolgee = useTolgee(['language'])
+const changeLanguage = (e) => {
+  tolgee.value.changeLanguage(e.target.value)
+}
+</script>
+<template>
+  <select :value="tolgee.getLanguage()" v-on:change="changeLanguage" class="selector">
+    <option value="en">🇬🇧 English</option>
+    <option value="fr">🇫🇷 Français</option>
+    <option value="es">🇪🇸 Español</option>
+  </select>
+</template>
